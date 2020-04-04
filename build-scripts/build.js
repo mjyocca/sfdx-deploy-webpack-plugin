c47212ad458a3efb8ed04489807b33ca6f1e0451
Michael Yocca <mjyocca@outlook.com>
2020-04-04 09:36:25 -0700

    Removed Unnecessary Files

--- build-scripts/build.js
+++ build-scripts/build.js
@@ -1,11 +1,7 @@
 const fs = require('fs');
 
-const esImport = `export * as SfdxDeployPlugin from './index.cjs';`;
+const esImport = `export * as SfdxDeployPlugin from './index.js';`;
 
 fs.writeFile('./build/index.mjs', esImport, (err) => {
-  console.log(err);
-});
-
-fs.rename('./build/index.js', './build/index.cjs', (err) => {
-  console.log(err);
+  if (err) console.error(err);
 });
